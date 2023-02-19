2c8bb16ada9236f08978ef7afe7fa10a0706b6ab
Anton Bezmelnitsin <a.bezmelnitsin.a@yandex.ru>
2023-02-19 22:04:19 +0500

    Removed null! from model

--- ElectricityMetering.BL/Model/Garage.cs
+++ ElectricityMetering.BL/Model/Garage.cs
@@ -10,8 +10,8 @@ namespace ElectricityMetering.BL.Model
     {
         public int Id { get; set; }
         public string Number { get; set; } = null!;
-        public string SealNumber { get; set; } = null!;
-        public string CounterNumber { get; set; } = null!;
+        public string? SealNumber { get; set; }
+        public string? CounterNumber { get; set; }
         public DateOnly SealingDate { get; set; }
     }
 }
