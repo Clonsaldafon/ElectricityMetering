f5bc674b7aaf4d6ab9f6c56fd6fcfdf01613030c
Anton Bezmelnitsin <a.bezmelnitsin.a@yandex.ru>
2023-02-19 20:51:01 +0500

    Creating ApplicationContext v0.1

--- ElectricityMetering.BL/ApplicationContext.cs
+++ ElectricityMetering.BL/ApplicationContext.cs
@@ -1,7 +1,23 @@
-﻿namespace ElectricityMetering.BL
-{
-    public class ApplicationContext
-    {
+﻿using Microsoft.EntityFrameworkCore;
+using ElectricityMetering.BL.Model;
 
+namespace ElectricityMetering.BL
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<Garage> Garages { get; set; }
+        public DbSet<Owner> Owners { get; set; }
+
+        private string _connectionString = $"Server=satao.db.elephantsql.com;Port=5432;Database=bowasjim;User Id=bowasjim;Password=9Cvmf5C8U79RKZ_madz-bs0PywwziGFl";
+
+        public ApplicationContext()
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql(_connectionString);
+        }
     }
 }
