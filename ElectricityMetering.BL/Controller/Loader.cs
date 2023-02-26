2f4aa1794c6eecd1836a266f52a37f0e2ac9f163
Anton Bezmelnitsin <a.bezmelnitsin.a@yandex.ru>
2023-02-26 19:26:31 +0500

    Creating data loader v0.1

--- ElectricityMetering.BL/Controller/Loader.cs
+++ ElectricityMetering.BL/Controller/Loader.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ElectricityMetering.BL.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,17 @@ namespace ElectricityMetering.BL.Controller
 {
     public class Loader
     {
-        
+        public void LoadInfo(int garageId)
+        {
+            using (ApplicationContext db = new ApplicationContext())
+            {
+                Garage? garage = db.Garages.FirstOrDefault(g => g.Id == garageId);
+                if (garage is not null)
+                {
+                    Owner? owner = db.Owners.FirstOrDefault(o => o.Garages.Contains(garage));
+                }
+                
+            }
+        }
     }
 }
